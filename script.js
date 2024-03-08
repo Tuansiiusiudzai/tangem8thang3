db0300f761727f2a0174f973bf74c15a32a73970
Tuansiiusiudzai <90133765+Tuansiiusiudzai@users.noreply.github.com>
2024-03-08 22:38:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// JavaScript source code
-document.getElementById('btn').addEventListener('click', function () {
-    // Thêm ho?c xóa l?p 'white-background' khi nút ???c nh?p
-    document.body.classList.toggle('white-background');
-
-    // T?o m?t ph?n t? m?i ch?a v?n b?n "Hello"
-    var helloText = document.createElement('div');
-    helloText.textContent = 'Hello';
-
-    // N?u n?n màu là tr?ng, hi?n th? v?n b?n "Hello"
-    if (document.body.classList.contains('white-background')) {
-        document.body.appendChild(helloText);
-    } else { // N?u không, xóa v?n b?n "Hello"
-        document.body.removeChild(helloText);
-    }
+document.getElementById('btn').addEventListener('click', function() {
+  var image = document.getElementById('image');
+  image.style.opacity = '0';
+  setTimeout(function() {
+    image.style.display = 'none';
+  }, 500);
+  document.body.style.backgroundColor = '#fff';
 });
