4a921d5db2ba30d65ee5891df88934a5ec0e750a
Tuansiiusiudzai <90133765+Tuansiiusiudzai@users.noreply.github.com>
2024-03-08 22:30:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // JavaScript source code
+document.getElementById('btn').addEventListener('click', function() {
+  alert('Button clicked!');
+});
+
 document.getElementById('btn').addEventListener('click', function () {
-    // Thêm ho?c xóa l?p 'white-background' khi nút ???c nh?p
+    // ThÃªm ho?c xÃ³a l?p 'white-background' khi nÃºt ???c nh?p
     document.body.classList.toggle('white-background');
 
     // T?o m?t ph?n t? m?i ch?a v?n b?n "Hello"
     var helloText = document.createElement('div');
     helloText.textContent = 'Hello';
 
-    // N?u n?n màu là tr?ng, hi?n th? v?n b?n "Hello"
+    // N?u n?n mÃ u lÃ  tr?ng, hi?n th? v?n b?n "Hello"
     if (document.body.classList.contains('white-background')) {
         document.body.appendChild(helloText);
-    } else { // N?u không, xóa v?n b?n "Hello"
+    } else { // N?u khÃ´ng, xÃ³a v?n b?n "Hello"
         document.body.removeChild(helloText);
     }
 });
